c36a9ff1a6ad760eb613035eacd776c3c4479efe
GuilhermeHorta22 <ghorta800@gmail.com>
2024-09-28 18:42:35 -0300

    manual feito e funcionando, só colocar a interface.

--- TorreDeHanoi.cpp
+++ TorreDeHanoi.cpp
@@ -34,32 +34,32 @@ void exibirTorres(TpPilhaM2 pm)
 char validarMovimento(TpPilhaM2 pm, int origem, int destino)
 {
 	int aux=1;
-    if (pilhaVazia(pm, origem))
+    if(pilhaVazia(pm,origem))
     {
         printf("\nTorre de origem vazia! Escolha outra torre\n");
         aux = -1;
     }
 	else
 	{
-    	int topoOrigem = elementoTopo(pm, origem);
+    	int topoOrigem = elementoTopo(pm,origem);
     	int topoDestino;
 
-        if (pilhaVazia(pm, destino))
+        if(pilhaVazia(pm,destino))
 		{
             topoDestino = 0; //***
-        } else
+        }
+		else
 		{
             topoDestino = elementoTopo(pm, destino);
         }
 
-        if (topoOrigem > topoDestino)
+        if(topoOrigem > topoDestino)
         {
             printf("\n[Movimento invalido!]\n Disco maior nao pode ficar sobre disco menor\n");
-            aux=-1;
-            getch();
+            aux = -1;
         }
 	}
-
+	getch();
     return aux;
 }
 
@@ -71,80 +71,78 @@ void moverDisco(TpPilhaM2 &pm, int origem, int destino)
 }
 
 //andressa
-void manual(TpPilhaM2 &pm)
-{
-	system("cls");
-	FormularioMenu();
-    int qtdeDisco, i = 0, valor, movimento = 0;
-    int sair = 0;
-    TpTorre t;
-	Layout("Modo Manual");
+//void manual(TpPilhaM2 &pm)
+//{
+//	system("cls");
+//	FormularioMenu();
+//    int qtdeDisco, i = 0, valor, movimento = 0;
+//    int sair = 0;
+//    TpTorre t;
+//	Layout("Modo Manual");
 
-	
-    printf("\nQuantidade de discos (MIN:3 - MAX:10): ");
-    scanf("%d", &qtdeDisco);
+//	
+//    printf("\nQuantidade de discos (MIN:3 - MAX:10): ");
+//    scanf("%d", &qtdeDisco);
 
-    if(qtdeDisco >= 3 && qtdeDisco <= 10)
-    {
-        t.origem = 0;
-        valor = qtdeDisco;
-        while (i < qtdeDisco)
-        {
-            inserir(pm, valor, 0); // origem
-            valor--;
-            i++;
-        }
+//    if(qtdeDisco >= 3 && qtdeDisco <= 10)
+//    {
+//        t.origem = 0;
+//        valor = qtdeDisco;
+//        while (i < qtdeDisco)
+//        {
+//            inserir(pm, valor, 0); // origem
+//            valor--;
+//            i++;
+//        }
 
-        exibirTorres(pm);
-        getch();
+//        exibirTorres(pm);
+//        getch();
 
-        printf("\nPara qual pilha voce deseja levar a torre? (2 - 3): ");
-        scanf("%d", &t.destino);
-        t.destino--;
+//        printf("\nPara qual pilha voce deseja levar a torre? (2 - 3): ");
+//        scanf("%d", &t.destino);
+//        t.destino--;
 
-		if (t.destino == 1) {
-            t.aux = 2;
-        } else {
-            t.aux = 1;
-        }
+//		if (t.destino == 1) {
+//            t.aux = 2;
+//        } else {
+//            t.aux = 1;
+//        }
 
-        printf("\n\n*** MOVIMENTANDO DA TORRE %d PARA TORRE %d\n", t.origem + 1, t.destino + 1);
+//        printf("\n\n*** MOVIMENTANDO DA TORRE %d PARA TORRE %d\n", t.origem + 1, t.destino + 1);
 
-        while (sair != -1 || !pilhaVazia(pm, t.origem) && !pilhaVazia(pm, t.aux)) //movimentacao
-        {
-            int origem, destino;
-            exibirTorres(pm);
-            printf("\n\nDe qual torre deseja mover o disco? (1, 2 ou 3): ");
-            scanf("%d", &origem);
-            origem--;
+//        while (sair != -1 || !pilhaVazia(pm, t.origem) && !pilhaVazia(pm, t.aux)) //movimentacao
+//        {
+//            int origem, destino;
+//            exibirTorres(pm);
+//            printf("\n\nDe qual torre deseja mover o disco? (1, 2 ou 3): ");
+//            scanf("%d", &origem);
+//            origem--;
 
-            printf("\nPara qual torre deseja mover o disco? (1, 2 ou 3): ");
-            scanf("%d", &destino);
-            destino--;
+//            printf("\nPara qual torre deseja mover o disco? (1, 2 ou 3): ");
+//            scanf("%d", &destino);
+//            destino--;
 
-            if (validarMovimento(pm, origem, destino))
-            {
-                moverDisco(pm, origem, destino);
-                movimento++;
-            }
+//            if (validarMovimento(pm, origem, destino))
+//            {
+//                moverDisco(pm, origem, destino);
+//                movimento++;
+//            }
 
-            if (pilhaVazia(pm, t.origem) && pilhaVazia(pm, t.aux) && !pilhaVazia(pm, t.destino)) //se chegou no fim
-            {
-                printf("\n\n*** Jogo completado com sucesso em [%d] movimentos! ***\n", movimento);
-                sair = -1;
-                getch();
-            }
-        }
-    }
-    else
-    {
-    	limpatela();
-        printf("\nQuantidade de discos invalida!\n");
-        getch();
-    }
-}
-
-
+//            if (pilhaVazia(pm, t.origem) && pilhaVazia(pm, t.aux) && !pilhaVazia(pm, t.destino)) //se chegou no fim
+//            {
+//                printf("\n\n*** Jogo completado com sucesso em [%d] movimentos! ***\n", movimento);
+//                sair = -1;
+//                getch();
+//            }
+//        }
+//    }
+//    else
+//    {
+//    	limpatela();
+//        printf("\nQuantidade de discos invalida!\n");
+//        getch();
+//    }
+//}
 
 
 //gui
@@ -166,84 +164,117 @@ void manual(TpPilhaM2 &pm)
 			valor--;
 			i++;
 		}
-	
-		//TESTE PARA SABER SE ESTÁ TUDO CERTO NO INSERIR
-		exibir(pm,t.origem);
-		getch();
-		
-		//opção 2-3 porque a torre já começa na 1
-        printf("\nPara qual torre deseja mover o disco? (2 ou 3): ");
-        scanf("%d",&t.destino);
-		t.destino--;//trantando o destino já que nas pilhas são 0,1,2
-		
-		if(t.destino == 1) //pilha 2
-			t.aux = 2;
-		else
-		if(t.destino == 2) //pilha 3
-			t.aux = 1;
-		else
-			printf("\nTorre invalida!\n");
-		
-		while(!pilhaVazia(pm,0) && !pilhaVazia(pm,t.aux))
+
+		printf("\nPara qual torre deseja mover o disco? (2 ou 3): ");
+	    scanf("%d",&t.destino);
+		t.destino--;
+		do
 		{
-			
-			/*
-			aqui teria que fazer a parte de perguntar a torre de onde ele vai tirar o disco e para qual torre
-			ele deseja levar, fazendo as validações se é possivel ou não fazer a movimentação.
-			A repetição vai se encerrar quando não houver mais disco na origem e tambem na auxiliar
-			isso indica que todos os disco estão devidamente na torre destino.
-				
-			OQUE FAZER NESSA REPETIÇÃO COLOCAMOS NO IF DE BAIXO
-			*/
-			if(pilhaVazia(pm,t.destino))
+			//VALIDAÇÃO PARA FAZER AS MOVIMENTAÇÕES
+       		if(pilhaVazia(pm,t.destino))
+       		{
 				inserir(pm,retirar(pm,t.origem),t.destino);
-			else
+       			printf("\nDisco movido com sucesso!");
+       			movimento++;
+       		}
+       		else
 			if(elementoTopo(pm,t.origem) < elementoTopo(pm,t.destino))
+			{
 				inserir(pm,retirar(pm,t.origem),t.destino);
-			
-			printf("\n\nDe qual torre deseja mover o disco? (1, 2 ou 3): ");
-	        scanf("%d",&t.origem);
-	        t.origem--;
-			printf("\nPara qual torre deseja mover o disco? (2 ou 3): ");
-       		scanf("%d",&t.destino);
-			t.destino--;	
+				printf("\nDisco movido com sucesso!");
+				movimento++;
+			}
+			else
+				printf("Erro! Nao foi possivel realizar o movimento!\n");
 				
-			movimento++; //contador para saber o total de movimentos
-		}
+			
+			//EXIBIÇÃO DAS PILHAS
+			if(pilhaVazia(pm,0))
+			{
+				printf("\nPilha vazia!"); 
+				printf("\nPILHA 1\n");
+			}
+			else
+			{
+				exibir(pm,0);
+				printf("\nPILHA 1\n");
+			}
+			
+			if(pilhaVazia(pm,1))
+			{
+				printf("\nPilha vazia!");
+				printf("\nPILHA 2\n");
+			}
+			else
+			{
+				exibir(pm,1);
+				printf("\nPILHA 2\n");
+			}
+			
+			if(pilhaVazia(pm,2))
+			{
+				printf("\nPilha vazia!");
+				printf("\nPILHA 3\n");
+			}
+			else
+			{
+				exibir(pm,2);
+				printf("\nPILHA 3\n");
+			}	
+			
+			//VERIFICANDO SE A PESSOA AINDA NÃO GANHOU PARA SEGUIR JOGANDO
+			if((!pilhaVazia(pm,0) && !pilhaVazia(pm,1))
+		 	|| (!pilhaVazia(pm,0) && !pilhaVazia(pm,2)) || (!pilhaVazia(pm,1) && !pilhaVazia(pm,2)))
+		 	{
+		 		printf("\n\nDe qual torre deseja mover o disco? (1, 2 ou 3): ");
+		        scanf("%d",&t.origem);
+		        t.origem--;
+				printf("\nPara qual torre deseja mover o disco? (1, 2 ou 3): ");
+	       		scanf("%d",&t.destino);
+				t.destino--;	
+		 	}
+		 	
+		 	//SE CASO A PESSOA CONSEGUIU MOVER TUDO
+		 	if((pilhaVazia(pm,0) && pilhaVazia(pm,1))
+		 	|| (pilhaVazia(pm,0) && pilhaVazia(pm,2)) || (pilhaVazia(pm,1) && pilhaVazia(pm,2)))
+		 		printf("\nPARABENS VOCE GANHOU!\n");
 		
-		
+		}while((!pilhaVazia(pm,0) && !pilhaVazia(pm,1))
+		 || (!pilhaVazia(pm,0) && !pilhaVazia(pm,2)) || (!pilhaVazia(pm,1) && !pilhaVazia(pm,2)));
+		 
+		 printf("\nTotal de movimentos: %d\n",movimento);
 	}
 	else
 		printf("\nQuantidade de disco invalida!\n");
 
 }
 
-void automatico(TpPilhaM2 &pm)
-{
-	int qtdeDisco, i=0, valor;
-	TpTorre t;
-	Layout("Modo Automatico");
-	printf("\nQuantidade de discos (MIN:3 - MAX:10): ");
-	scanf("%d",&qtdeDisco);
-	if(qtdeDisco >= 3 && qtdeDisco <= 10)
-	{
-		t.origem=0;
-		valor = qtdeDisco;
-		while(i<qtdeDisco)
-		{
-			inserir(pm,valor,0);
-			valor--;
-			i++;
-		}
-					
-		/*TESTE PARA SABER SE ESTÁ TUDO CERTO NO INSERIR
-		exibir(pm,t.origem);
-		getch(); */
-					
-	}
-	else
-		printf("\nQuantidade de disco invalida!\n");
-}
+//void automatico(TpPilhaM2 &pm)
+//{
+//	int qtdeDisco, i=0, valor;
+//	TpTorre t;
+//	Layout("Modo Automatico");
+//	printf("\nQuantidade de discos (MIN:3 - MAX:10): ");
+//	scanf("%d",&qtdeDisco);
+//	if(qtdeDisco >= 3 && qtdeDisco <= 10)
+//	{
+//		t.origem=0;
+//		valor = qtdeDisco;
+//		while(i<qtdeDisco)
+//		{
+//			inserir(pm,valor,0);
+//			valor--;
+//			i++;
+//		}
+//					
+//		/*TESTE PARA SABER SE ESTÁ TUDO CERTO NO INSERIR
+//		exibir(pm,t.origem);
+//		getch(); */
+//					
+//	}
+//	else
+//		printf("\nQuantidade de disco invalida!\n");
+//}
 
 int main(void)
 {
@@ -271,12 +302,12 @@ int main(void)
 				break;
 				
 			case 'B':
-				automatico(pm);
+				//automatico(pm);
 				
 				break;
 				
 			case 27:
-				printf("\nJogo encerrado!\n"); //pode colocar na parte de mensagem do jogo
+				printf("\nJJogo encerrado!\n"); //pode colocar na parte de mensagem do jogo
 		}
 		getch();
 	}while(op != 27);
